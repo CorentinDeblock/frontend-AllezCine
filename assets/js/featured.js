acc9108c40b36ba550559116dc22c38b7f1d2ec6
Cindy Buchet <mllcindy@hotmail.com>
2019-06-19 16:51:45 +0200

    debut API

--- assets/js/featured.js
+++ assets/js/featured.js
@@ -2,13 +2,18 @@ let card = document.querySelector("#card");
 let mov = card.content.querySelector("#mov");
 let target = document.getElementById("target");
 
-fetch('assets/js/movies.json').then(blob => {
+fetch("https://api.themoviedb.org/3/movie/157336?api_key=b87d7664b3a521ad14526491b0c37dc9&append_to_response=videos,credits").then(blob => {
     return blob.json();
 }).then(value => {
-    let mydata = value;
-
-
+    let mydata = [];
+    if(typeof(value) == "array"){
+        mydata = value;
+    }else{
+        mydata.push(value);
+    }
+    console.log(mydata)
     for(let i = 0;i < mydata.length; i++){
+        console.log(mydata[i])
         let a = document.importNode(mov, true);
         let imag = a.querySelector(".card-img-top");
         let h = a.querySelector(".card__name");
@@ -20,20 +25,48 @@ fetch('assets/js/movies.json').then(blob => {
         let desc = a.querySelector(".descri");
         let realis = a.querySelector(".reali");
         let act = a.querySelector(".acteurs");
-        let vid = a.querySelector(".video");
+        let video = a.querySelector(".video");
 
+        let type = "";
+        for(let j = 0;j < mydata[i].genres.length;j++){
+            if ( j === mydata[i].genres.length - 1){
+                type += mydata[i].genres[j].name;
+            } else {
+                type += mydata[i].genres[j].name + ', ';
+            }
+        }
 
-        imag.src = mydata[i].img;
-        h.innerHTML = mydata[i].name;
-        year.innerHTML = mydata[i].annee;
-        genr.innerHTML = mydata[i].genre;
-        motit.innerHTML = mydata[i].name;
+        let casting = mydata[i].credits.cast;
+        let crew  = mydata[i].credits.crew;
+        let vid = mydata[i].videos.results;
+
+        let actor = [];
+        let realisator = [];
+
+        for(let j = 0; j < casting.length;j++){
+            if(casting[j].character != undefined){
+                actor.push(casting[j].name);
+            }
+        }
+        for(let j = 0; j < crew.length;j++){
+            if(crew[j].job == "Director"){
+                realisator.push(crew[j].name);
+            }
+        }
+
+        let date = new Date(mydata[i].release_date);
+
+        imag.src = "https://image.tmdb.org/t/p/w500" + mydata[i].poster_path;
+        h.innerHTML = mydata[i].title;
+        year.innerHTML = date.getFullYear();
+        genr.innerHTML = type;
+        motit.innerHTML = mydata[i].title;
         bu.setAttribute("data-target", "#film"+i);
         idd.id = "film"+i;
-        desc.innerHTML = mydata[i].descrip;
-        realis.innerHTML = mydata[i].realisateur;
-        act.innerHTML = mydata[i].acteurs;
-        vid.src = mydata[i].trailerYtb;
+        desc.innerHTML = mydata[i].overview;
+        realis.innerHTML = realisator;
+        act.innerHTML = actor;
+        video.src = "https://www.youtube.com/embed/" + vid[vid.length - 1].key;
         target.appendChild(a);
     }
 
